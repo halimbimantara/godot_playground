e779d938f4d0a62fcb9ef501d05562dce4198441
Jeferson 'Shin <shin.jef@gmail.com>
2021-06-16 21:58:20 -0300

    Added weighted drop on the list

--- Seven_GameJam/misc/Vase.gd
+++ Seven_GameJam/misc/Vase.gd
@@ -7,16 +7,21 @@ var Orb = preload("res://Misc/Orb.tscn")
 var Potion = preload("res://Misc/Potion.tscn")
 
 func choose_drop() -> PackedScene:
-	var options = [Coin, Orb, Potion]
+	var options = [
+		Coin, Coin, Coin, Coin, Coin, 
+		Orb, Orb, Orb, 
+		Potion
+	]
+	
 	options.shuffle()
 	return options.pop_front()
+
 
 func drop_item():
 	var random_item = choose_drop()
 	var random_instance = Utils.instance_scene_on_main(random_item, global_position)
 	random_instance.global_position.x += rand_range(-4, 4)
 	random_instance.global_position.y += rand_range(-4, 0)
-
 
 func _on_Hurtbox_hit(damage) -> void:
 	drop_item()
