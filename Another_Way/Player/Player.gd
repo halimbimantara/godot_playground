751b3ebe39c2d98d226ab2975786811af7980080
Jeferson 'Shin <shin.jef@gmail.com>
2021-07-10 16:27:25 +0200

    Added dash action to player

--- Another_Way/Player/Player.gd
+++ Another_Way/Player/Player.gd
@@ -11,15 +11,21 @@ export (int) var MAX_SPEED = 70
 export (float) var FRICTION = 0.3
 export (int) var GRAVITY = 400
 export (int) var JUMP_FORCE = 160
+export (int) var DASH_FORCE = 2400
 export (PackedScene) var dustEffectScene
+export (PackedScene) var dashEffectScene
 
 onready var animationPlayer: AnimationPlayer = $AnimationPlayer
 onready var dustEffectPosition: Position2D = $DustEffectPosition
 onready var frontFloor: RayCast2D = $Senses/FrontFloor
 onready var rearFloor: RayCast2D = $Senses/RearFloor
+onready var topDashSense: RayCast2D = $Senses/TopDash
+onready var bottomDashSense: RayCast2D = $Senses/BottomDash
 onready var coyoteTimer: Timer = $Senses/CoyoteTimer
+onready var dashTimer: Timer = $Senses/DashTimer
+onready var dashTimerEffect: Timer = $Senses/DashTimerEffect
 
-enum { MOVE, JUMP, FALLING, ATTACK, HIT, DEATH }
+enum { MOVE, JUMP, FALLING, DASH, ATTACK, HIT, DEATH }
 
 var life: int = MAX_HITPOINTS
 var coins := 0
@@ -30,7 +36,7 @@ var motion := Vector2.ZERO
 var flip_direction: int = 1
 var is_jumping := false
 var is_double_jump := false
-
+var is_dashing := false
 
 func _ready():
 	emit_signal("life_changed", life, MAX_HITPOINTS)
@@ -42,20 +48,26 @@ func _physics_process(delta: float) -> void:
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	
-	if life <= 0:
-		_change_state(DEATH)
+	clean_state_flags()
 	
 	match state:
 		MOVE: _state_move(input_vector, delta)
 		JUMP: _state_jump(input_vector, delta)
 		FALLING: _state_falling(input_vector, delta)
 		ATTACK: _state_attack(input_vector, delta)
+		DASH: _state_dash(input_vector, delta)
 		HIT: _state_hit(input_vector, delta)
 		DEATH: _state_death(input_vector)
-
+	
 
 func _input(event: InputEvent):
 	if not state == HIT or not state == DEATH:
+		if event.is_action_pressed("dash") and not is_dashing and not is_grounded():
+			if not state == DASH and mana > 0:
+				change_mana(-1)
+				is_dashing = true
+				_change_state(DASH)
+		
 		if event.is_action_pressed("jump") and not is_jumping:
 			if state == FALLING and coyoteTimer.time_left > 0:
 				is_jumping = true
@@ -74,7 +86,7 @@ func _input(event: InputEvent):
 				_change_state(JUMP)
 			
 		if event.is_action_pressed("attack"):
-			if not state == ATTACK:
+			if not state == ATTACK and not state == DASH:
 				_change_state(ATTACK)
 
 
@@ -98,6 +110,18 @@ func _state_hit(input: Vector2, delta: float):
 	move()
 
 
+func _state_dash(input: Vector2, delta: float):
+	animationPlayer.play("Dash")
+	motion.y = 0
+	
+	if is_grounded() or is_front_sense_touching():
+		_change_state(MOVE)
+	
+	create_dash_effect()
+	apply_dash_force(delta)
+	move()
+
+
 func _state_attack(input: Vector2, delta: float):
 	animationPlayer.play("Attack")
 	input = Vector2.ZERO
@@ -113,7 +137,6 @@ func _state_falling(input: Vector2, delta: float):
 	animationPlayer.play("Falling")
 	
 	if is_grounded():
-		reset_jump()
 		_change_state(MOVE)
 	
 	apply_gravity(delta)
@@ -154,15 +177,26 @@ func _state_move(input: Vector2, delta: float):
 
 
 func _change_state(new_state):
+	if new_state == state:
+		return
+
 	match new_state:
 		FALLING:
 			coyoteTimer.start()
+		DASH:
+			dashTimer.start()
 	
 	state = new_state
 
 
 func is_grounded() -> bool:
 	if frontFloor.is_colliding() or rearFloor.is_colliding():
+		return true
+	return false
+
+
+func is_front_sense_touching() -> bool:
+	if topDashSense.is_colliding() or bottomDashSense.is_colliding():
 		return true
 	return false
 
@@ -182,6 +216,11 @@ func apply_horizontal_force(input: Vector2, delta: float):
 	if input.x != 0:
 		motion.x += input.x * ACCELERATION * delta
 		motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
+
+
+func apply_dash_force(delta: float):
+	motion.x += flip_direction * DASH_FORCE * delta
+	motion.x = clamp(motion.x, -MAX_SPEED * 3, MAX_SPEED * 3)
 
 
 func apply_flip_scale(input: Vector2):
@@ -224,9 +263,14 @@ func apply_jump():
 	motion.y = -JUMP_FORCE
 
 
-func reset_jump():
-	is_jumping = false
-	is_double_jump = false
+func clean_state_flags():
+	if life <= 0:
+		_change_state(DEATH)
+	
+	if is_grounded():
+		is_jumping = false
+		is_double_jump = false
+		is_dashing = false
 
 
 func create_dust_effect():
@@ -235,14 +279,27 @@ func create_dust_effect():
 	instance.global_position = dustEffectPosition.global_position
 
 
+func create_dash_effect():
+	if dashTimerEffect.time_left == 0:
+		dashTimerEffect.start()
+		var instance = dashEffectScene.instance()
+		get_tree().current_scene.add_child(instance)
+		instance.scale.x *= flip_direction
+		instance.global_position = dustEffectPosition.global_position
+
+
+func _on_DashTimer_timeout() -> void:
+	if not state == HIT or not state == DEATH:
+		motion.x = 0
+		_change_state(MOVE)
+
+
 func _on_AnimationPlayer_animation_finished(anim_name):
 	match anim_name:
 		"Attack":
 			_change_state(MOVE)
 		
 		"Hit":
-			reset_jump()
-			
 			if life > 0:
 				_change_state(MOVE)
 			else: 
@@ -270,4 +327,3 @@ func _on_PickableRange_area_entered(pickable: Area2D):
 			"Coin":
 				change_coin(1)
 				pickable.collect()
-
