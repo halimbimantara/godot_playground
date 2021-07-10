76ffe535f21d505896308fb2168869e53a868c94
Jeferson 'Shin <shin.jef@gmail.com>
2021-07-10 16:52:55 +0200

    Renamed door sense to button

--- Another_Way/Environment/Button.gd
+++ Another_Way/Environment/Button.gd
@@ -1,19 +1,21 @@
 extends Area2D
 
-signal entered_door_sense()
-signal exited_door_sense()
+signal pressed_in()
+signal pressed_out()
 
 onready var animationPlayer: AnimationPlayer = $AnimationPlayer
+
 
 func _ready() -> void:
 	animationPlayer.play("idle")
 
+
 func _on_DoorSense_body_entered(body: Node) -> void:
 	animationPlayer.play("pushed")
-	emit_signal("entered_door_sense")
+	emit_signal("pressed_in")
 
 
 func _on_DoorSense_body_exited(body: Node) -> void:
 	if get_overlapping_bodies().size() < 1:
 		animationPlayer.play("idle")
-		emit_signal("exited_door_sense")
+		emit_signal("pressed_out")
